9f20587b3f2ba2e94c812c6dc49b2061daea9dbe
Admin <admin@192.168.1.13>
2026-05-22 20:27:14 +0700

    fix railway bind

--- server.js
+++ server.js
@@ -277,7 +277,7 @@ async function startServer(){
     const PORT =
     process.env.PORT || 8080
 
-    app.listen(PORT,()=>{
+    app.listen(PORT, "0.0.0.0", ()=>{
 
       console.log(
         `Server running on port ${PORT}`
